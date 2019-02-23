b9f0f2f7f0d8f03d9ebc45c3bce7090bce46c402
kuantzuhsiang <kuan.tzuhsiang@u.nus.edu>
2019-02-23 16:13:51 +0100

    able to record and read audio signals

--- app/src/main/java/e/eric/morsecode/MainActivity.java
+++ app/src/main/java/e/eric/morsecode/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private AudioRecord audioRecorder;
+    private static short[] audioData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,25 +21,42 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView start = findViewById(R.id.start);
 
+        final int bufferSize = AudioRecord.getMinBufferSize(44100, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
         audioRecorder = new AudioRecord(
                 MediaRecorder.AudioSource.MIC,
                 44100,
                 AudioFormat.CHANNEL_IN_MONO,
                 AudioFormat.ENCODING_PCM_16BIT,
-                AudioRecord.getMinBufferSize(44100, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT));
+                bufferSize);
 
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (audioRecorder.getState() == AudioRecord.STATE_INITIALIZED) {
-                    Log.d(TAG, "test 1");
-//                    audioRecorder.startRecording();
-                    short[] audioData;
-                    audioRecorder.read(audioData, );
-                    Log.d(TAG, "test 2");
+                    audioData = new short[bufferSize];
+                    audioRecorder.startRecording();
+                    if (audioRecorder.getRecordingState() == AudioRecord.RECORDSTATE_RECORDING) {
+                        Log.d(TAG, "test");
+                    }
+                    Thread recordingThread = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            while(true) {
+                                audioRecorder.read(audioData, 0, bufferSize);
+                                printRecording(audioData);
+                            }
+                        }
+                    });
+                    recordingThread.start();
                 }
             }
         });
 
+    }
+
+    private void printRecording(short[] data) {
+        for (int i = 0; i < data.length; i++) {
+            Log.d(TAG, i + ": " + Short.toString(data[i]));
+        }
     }
 }
