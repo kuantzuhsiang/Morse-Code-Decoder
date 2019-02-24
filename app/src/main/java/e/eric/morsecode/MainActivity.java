0f30ce68c6ab799620bbfa64311af0382d60dafa
kuantzuhsiang <kuan.tzuhsiang@u.nus.edu>
2019-02-24 06:59:54 +0100

    switch for alphabet and numbers

--- app/src/main/java/e/eric/morsecode/MainActivity.java
+++ app/src/main/java/e/eric/morsecode/MainActivity.java
@@ -11,6 +11,9 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -19,6 +22,10 @@ public class MainActivity extends AppCompatActivity {
     private int bufferSize;
     private boolean signalHighFlag = false;
     private int count = 0;
+    private TextView message;
+    private View dot, dash;
+    String messageText = "";
+    String messageWord = "";
 
     @Override
     @SuppressLint("ClickableViewAccessibility")
@@ -27,6 +34,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final ImageView start = findViewById(R.id.start);
+        message = findViewById(R.id.message);
+        dot = findViewById(R.id.dot);
+        dash = findViewById(R.id.dash);
 
         bufferSize = AudioRecord.getMinBufferSize(44100, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
         Log.d(TAG, "buffer size = " + bufferSize);
@@ -75,15 +85,19 @@ public class MainActivity extends AppCompatActivity {
     };
 
     private void checkForHigh(short[] data) {
-        if (Math.abs(data[0]) > 50) {
+        if (Math.abs(data[0]) > 100) {
+            Log.e(TAG, "signal detected");
             if (signalHighFlag) {
                 count++;
             } else {
                 determineTypeOfSilence(count);
+                messageWord = "";
                 signalHighFlag = true;
                 count = 1;
             }
         } else {
+            dot.setVisibility(View.GONE);
+            dash.setVisibility(View.GONE);
             if (!signalHighFlag) {
                 count++;
             } else {
@@ -96,19 +110,141 @@ public class MainActivity extends AppCompatActivity {
 
     private void determineTypeOfSilence(int count) {
         if (count <= 4) {
-            //next signal
+            //next dot/dash
+            Log.e(TAG, "next dot/dash");
         } else if (count <= 12) {
             //next letter
+            Log.e(TAG, "decode");
+            decodeLetter();
+            message.setText(messageText);
+            messageWord = "";
         } else {
             //next word
+            messageText += " ";
         }
     }
 
     private void determineTypeOfSignal(int count) {
         if (count <= 4) {
             //dot
+            dot.setVisibility(View.VISIBLE);
+            messageWord += ".";
         } else {
             //dash
+            dash.setVisibility(View.VISIBLE);
+            messageWord += "-";
+        }
+    }
+
+    private void decodeLetter() {
+        switch (messageWord) {
+            case ".-":
+                messageText += "A";
+                break;
+            case "-...":
+                messageText += "B";
+                break;
+            case "-.-.":
+                messageText += "C";
+                break;
+            case "-..":
+                messageText += "D";
+                break;
+            case ".":
+                messageText += "E";
+                break;
+            case "..-.":
+                messageText += "F";
+                break;
+            case "--.":
+                messageText += "G";
+                break;
+            case "....":
+                messageText += "H";
+                break;
+            case "..":
+                messageText += "I";
+                break;
+            case ".---":
+                messageText += "J";
+                break;
+            case "-.-":
+                messageText += "K";
+                break;
+            case ".-..":
+                messageText += "L";
+                break;
+            case "--":
+                messageText += "M";
+                break;
+            case "-.":
+                messageText += "N";
+                break;
+            case "---":
+                messageText += "O";
+                break;
+            case ".--.":
+                messageText += "P";
+                break;
+            case "--.-":
+                messageText += "Q";
+                break;
+            case ".-.":
+                messageText += "R";
+                break;
+            case "...":
+                messageText += "S";
+                break;
+            case "-":
+                messageText += "T";
+                break;
+            case "..-":
+                messageText += "U";
+                break;
+            case "...-":
+                messageText += "V";
+                break;
+            case ".--":
+                messageText += "W";
+                break;
+            case "-..-":
+                messageText += "X";
+                break;
+            case "-.--":
+                messageText += "Y";
+                break;
+            case "--..":
+                messageText += "Z";
+                break;
+            case ".----":
+                messageText += "1";
+                break;
+            case "..---":
+                messageText += "2";
+                break;
+            case "...--":
+                messageText += "3";
+            case "....-":
+                messageText += "4";
+                break;
+            case ".....":
+                messageText += "5";
+                break;
+            case "-....":
+                messageText += "6";
+                break;
+            case "--...":
+                messageText += "7";
+                break;
+            case "---..":
+                messageText += "8";
+                break;
+            case "----.":
+                messageText += "9";
+                break;
+            case "-----":
+                messageText += "0";
+                break;
         }
     }
 
