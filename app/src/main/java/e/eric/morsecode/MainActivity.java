53fcf290d99f6c8136bc55968f93f6b2d9a47f73
kuantzuhsiang <kuan.tzuhsiang@u.nus.edu>
2019-02-24 02:38:25 +0100

    added handler to sample every 25ms

--- app/src/main/java/e/eric/morsecode/MainActivity.java
+++ app/src/main/java/e/eric/morsecode/MainActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,24 +12,24 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 
-import java.util.Timer;
-
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private AudioRecord audioRecorder;
     private static short[] audioData;
-    private boolean recording = false;
-    private Timer timer;
+    private Handler handler = new Handler();
+    private int bufferSize;
 
-    @SuppressLint("ClickableViewAccessibility")
     @Override
+    @SuppressLint("ClickableViewAccessibility")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         final ImageView start = findViewById(R.id.start);
 
-        final int bufferSize = AudioRecord.getMinBufferSize(44100, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
+        bufferSize = AudioRecord.getMinBufferSize(44100, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
+        Log.d(TAG, "buffer size = " + bufferSize);
+
         audioRecorder = new AudioRecord(
                 MediaRecorder.AudioSource.MIC,
                 44100,
@@ -42,45 +43,34 @@ public class MainActivity extends AppCompatActivity {
                 switch (motionEvent.getAction()) {
                     case MotionEvent.ACTION_DOWN:
                         Log.d(TAG, "pressed");
-                        recording = true;
                         start.setImageResource(R.drawable.mic_pressed);
+                        recordSound.run();
                         return true;
                     case MotionEvent.ACTION_UP:
                         Log.d(TAG, "released");
-                        recording = false;
                         start.setImageResource(R.drawable.mic);
+                        handler.removeCallbacks(recordSound);
                         return true;
                 }
                 return false;
             }
         });
-
-//        start.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                start.setImageResource(R.drawable.mic_pressed);
-//
-//                if (audioRecorder.getState() == AudioRecord.STATE_INITIALIZED) {
-//                    audioData = new short[bufferSize];
-//                    audioRecorder.startRecording();
-//                    if (audioRecorder.getRecordingState() == AudioRecord.RECORDSTATE_RECORDING) {
-//                        Log.d(TAG, "test");
-//                    }
-//                    Thread recordingThread = new Thread(new Runnable() {
-//                        @Override
-//                        public void run() {
-//                            while(true) {
-//                                audioRecorder.read(audioData, 0, bufferSize);
-//                                printRecording(audioData);
-//                            }
-//                        }
-//                    });
-//                    recordingThread.start();
-//                }
-//            }
-//        });
-
     }
+
+    private Runnable recordSound = new Runnable() {
+        @Override
+        public void run() {
+            if (audioRecorder.getState() == AudioRecord.STATE_INITIALIZED) {
+                audioData = new short[bufferSize];
+                audioRecorder.startRecording();
+                if (audioRecorder.getRecordingState() == AudioRecord.RECORDSTATE_RECORDING) {
+                    audioRecorder.read(audioData, 0, bufferSize);
+//                    printRecording(audioData);
+                    handler.postDelayed(this, 25);
+                }
+            }
+        }
+    };
 
     private void printRecording(short[] data) {
         for (int i = 0; i < data.length; i++) {
